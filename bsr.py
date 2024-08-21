61a91fcd4843f4eae1730713b5fd9d2dee8c8fe4
RACE19 <race201909@outlook.com>
2024-08-21 10:27:17 +0000

    modified:   bsr.py

--- bsr.py
+++ bsr.py
@@ -15,7 +15,7 @@ subprocess.Popen(r'C:\Program Files\Star Rail\Game\StarRail.exe', shell=True)
 # 初始化OCR引擎
 ocr = PaddleOCR(use_angle_cls=True, lang='ch',
                 use_gpu=True, show_log=False)
-
+# 配置
 slice = {'horizontal_stride': 300, 'vertical_stride': 500,
          'merge_x_thres': 50, 'merge_y_thres': 35}
 
